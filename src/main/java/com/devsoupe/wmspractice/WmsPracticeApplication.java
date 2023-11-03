80b42a18268798ca4ac9414524c024324595846c
devsoupe <luvyum4e@gmail.com>
2023-11-04 01:42:45 +0900

    Initial commit

---
+++ src/main/java/com/devsoupe/wmspractice/WmsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.devsoupe.wmspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsoupe/wmspractice/WmsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsoupe.wmspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
