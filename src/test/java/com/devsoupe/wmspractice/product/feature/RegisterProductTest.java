16079d1d55e726f96161e5786707f5e84457f964
devsoupe <luvyum4e@gmail.com>
2023-11-04 01:53:38 +0900

    2. create test case registerProduct

--- src/test/java/com/devsoupe/wmspractice/product/feature/RegisterProductTest.java
+++ src/test/java/com/devsoupe/wmspractice/product/feature/RegisterProductTest.java
@@ -1,4 +1,12 @@
 package com.devsoupe.wmspractice.product.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterProductTest {
+
+    @Test
+    @DisplayName("상품을 등록한다.")
+    void registerProdcut() {
+    }
 }
