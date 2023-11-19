94dcc86a6f86e5d406a19602310929cbfe581d6a
devsoupe <luvyum4e@gmail.com>
2023-11-19 22:53:34 +0900

    5. move inner classes to upper level

---
+++ src/test/java/com/devsoupe/wmspractice/product/feature/Category.java
@@ -0,0 +1,11 @@
+package com.devsoupe.wmspractice.product.feature;
+
+public enum Category {
+    ELECTRONICS("전자 제품");
+
+    private final String description;
+
+    Category(final String description) {
+        this.description = description;
+    }
+}
